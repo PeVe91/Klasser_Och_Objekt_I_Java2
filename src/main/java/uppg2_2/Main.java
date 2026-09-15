d014fe30e94e8d682ccc81c4e9587b1536a44d34
PeVe91 <321365708+PeVe91@users.noreply.github.com>
2026-09-15 13:14:20 +0200

    Update Main.java

--- src/main/java/uppg2_2/Main.java
+++ src/main/java/uppg2_2/Main.java
@@ -1,4 +1,13 @@
 package uppg2_2;
 
 public class Main {
+    static void main() {
+
+        Triangle t1 = new Triangle(10, 15, 20);
+        Triangle t2 = new Triangle(t1);
+
+        t1.setSideA(66);
+
+        System.out.println(t2.getSideA());
+    }
 }
